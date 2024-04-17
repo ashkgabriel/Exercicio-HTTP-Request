2355034ba8eec4b234255580a7c59af690fab934
Gabriel Fortes Ashikaga <gabriel.ashikaga@fatec.sp.gov.br>
2024-04-16 22:12:59 -0300

    Finalizando

--- index.js
+++ index.js
@@ -12,25 +12,21 @@ app.listen(port, () => {
 let alunos = []
 let habilidades = []
 
-// let alunos = [
-// {
-// "ra": 123, 
-// "nome": "Diego", 
-// "turma": "ADS",
-// "habilidades": ['Javascript', 'ReactJS', 'Redux']
-// },
-//     {
-//         "ra": 123, 
-//         "nome": "Leandro", 
-//         "turma": "DSM",
-//         "habilidades": ['VueJS', 'Ruby on Rails', 'Node']
-//     }
-// ]
-
 // Lista todos os alunos
 
 app.get('/alunos', (req, res) => {
-    res.json(alunos)
+    res.json(alunos.map(({ ra, nome, turma }) => ({ ra, nome, turma })))
+})
+
+// Lista aluno filtrado pelo RA
+
+app.get('/alunos/:ra', (req, res) => {
+    const ra = parseInt(req.params.ra)
+    const index = alunos.find(x => x.ra == ra)
+
+    if (index) {
+        res.json({ nome: index.nome, turma: index.turma, habilidades: index.habilidades })
+    } else return res.status(400).json({ error: 'Aluno não encontrado..' })
 })
 
 // POST - Adiciona um aluno na lista
@@ -42,27 +38,69 @@ app.post('/alunos', (req, res) => {
 
 // POST - Adiciona um curso para o aluno
 
-app.post('/alunos/adiciona-curso/', (req, res) => {
-    const index = alunos.findIndex(x => x.ra == req.query.ra)
-
-    alunos.push(req.body.habilidades)
-    res.json(req.body)
+app.post('/alunos/:ra/adiciona-curso/', (req, res) => {
+    const ra = parseInt(req.params.ra);
+    const habilidade = req.body.habilidade;
+    const aluno = alunos.find(a => a.ra === ra);
+    if (!aluno) {
+        return res.status(404).json({ error: 'Aluno não encontrado.' });
+    }
+   
+    aluno.habilidades.push(habilidade);
+    res.status(201).json(aluno);
 })
 
 // PUT - Alterar os dados de um aluno através do RA
 
-app.put('/alunos/', (req, res) => {
-    const index = alunos.findIndex(x => x.ra == req.query.ra)
-    alunos[index] = { ra: req.body.ra, nome: req.body.nome, turma: req.body.turma, habilidades: alunos[index].habilidades }
-    res.send(JSON.stringify(alunos[index]))
+app.put('/alunos/:ra', (req, res) => {
+    const ra = parseInt(req.params.ra);
+    const novosDados = req.body;
+    const index = alunos.findIndex(a => a.ra === ra);
+    if (index === -1) {
+        return res.status(404).json({ error: 'Aluno não encontrado.' });
+    }
+    alunos[index] = { ...alunos[index], ...novosDados };
+    res.json(alunos[index]);
 })
 
 // PUT - Alterar os dados dos cursos do aluno
 
-app.put('/alunos/habilidades/', (req, res) => {
-    const index = alunos.findIndex(x => x.ra == req.query.ra)
-    alunos[index] = { habilidades: habilidades.push(req.body.habilidades) }
-    res.send(JSON.stringify(alunos[index]))
+app.put('/alunos/:ra/habilidades/', (req, res) => {
+    const ra = parseInt(req.params.ra);
+    const habilidades = req.body.habilidades;
+    const aluno = alunos.find(a => a.ra === ra);
+    if (!aluno) {
+        return res.status(404).json({ error: 'Aluno não encontrado.' });
+    }
+    aluno.habilidades = habilidades;
+    res.json(aluno);
 })
 
-// 
+// DELETE - Deleta um aluno
+
+app.delete('/alunos/:ra', (req, res) => {
+    const ra = parseInt(req.params.ra);
+    const index = alunos.findIndex(a => a.ra === ra);
+    if (index === -1) {
+        return res.status(404).json({ error: 'Aluno não encontrado.' });
+    }
+    alunos.splice(index, 1);
+    res.sendStatus(204);
+})
+
+// DELETE - Deleta uma habilidade de um aluno
+
+app.delete('/alunos/:ra/habilidades/:habilidade', (req, res) => {
+    const ra = parseInt(req.params.ra);
+    const habilidade = req.params.habilidade;
+    const aluno = alunos.find(a => a.ra === ra);
+    if (!aluno) {
+        return res.status(404).json({ error: 'Aluno não encontrado.' });
+    }
+    const index = aluno.habilidades.indexOf(habilidade);
+    if (index === -1) {
+        return res.status(404).json({ error: 'Habilidade não encontrada para este aluno.' });
+    }
+    aluno.habilidades.splice(index, 1);
+    res.sendStatus(204);
+})
